c5e820b8097ae387d7b3d65af4093f4e4cf4c598
German Nicolas Olle <germanolle.unlp@gmail.com>
2017-03-21 17:43:29 -0300

    Reduce project

--- client/app/app.js
+++ client/app/app.js
@@ -16,5 +16,4 @@ angular.module('app', [
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
   })
-
   .component('app', AppComponent);
